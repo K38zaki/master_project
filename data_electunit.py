5583e943b2435dc0370be7b57a94f8539c636d24
K38zaki <miyaken0613@gmail.com>
2021-09-12 02:46:37 +0900

    built_ elec_unit_data

--- data_electunit.py
+++ data_electunit.py
@@ -4,11 +4,11 @@ import warnings
 warnings.simplefilter('ignore')
 
 def initial_setting(pref_num):
-    pref_1 = pd.read_csv("elec_data/gikai_pref_"+str(pref_num)+".csv")
+    pref_1 = pd.read_csv("original_candidates_data/gikai_pref_"+str(pref_num)+".csv")
     pref_1["id"] = pref_1.index
-    ele_ymd_l = pref_1["ele_ymd"].unique()
-    circle_base = pd.DataFrame(pref_1["circle"].unique(),columns=["circle"])
-    return pref_1, ele_ymd_l, circle_base
+    #ele_ymd_l = pref_1["ele_ymd"].unique()
+    #circle_base = pd.DataFrame(pref_1["circle"].unique(),columns=["circle"])
+    return pref_1 #ele_ymd_l, circle_base
 
 def asitis_all_eles(pref_1):
     ## as it is (元csvからそのまま使える変数)　→　groupby().first()で取得してしまう
@@ -17,20 +17,18 @@ def asitis_all_eles(pref_1):
     asitis_l = ['election', 'prefecture', 'municipality','voting_ymd', 'year', 'month', 'day', 'kokuji_ymd','e_voting_rate','e_seats_target','e_n_candidates','e_competitive_ratio','e_all_voter', 'e_male_all_voter','e_female_all_voter','e_change_all_voter', 'e_reason', 'e_pre_v_rate','e_pre_seats_t', 'e_pre_n_cand', 'e_pre_compe_rate']
     
     #各選挙の一人目の候補者の行から、その選挙に関するデータを取得
-    asitis_df = pref_1.groupby("ele_ymd").first()[asitis_l].reset_index()
+    asitis_df = pref_1.groupby("ele_ymd").first()[asitis_l]
     
     #変数名変更
-    columns_name = ['election', 'prefecture', 'municipality','ymd', 'year', 'month', 'day', 'kokuji_ymd','voting_rate','n_seats_display','n_cand_display','compe_ratio_display','n_voters_inlaw', 'male_voters_inlaw','female_voters_inlaw','change_voters_inlaw', 'reason', 'pre_voting_rate','pre_seats_display', 'pre_n_cand_display', 'pre_compe_rate_display']
-    
+    columns_name = ['election','prefecture','municipality','ymd','year','month','day', 'kokuji_ymd','voting_rate','n_seats_display','n_cand_display','compe_ratio_display','n_voters_inlaw', 'male_voters_inlaw','female_voters_inlaw','change_voters_inlaw', 'reason', 'pre_voting_rate','pre_seats_display', 'pre_n_cand_display', 'pre_compe_rate_display']
     asitis_df.columns = columns_name
     
-    return asitis_df
+    ele_ymd_l = list(asitis_df.index)
+    
+    return asitis_df, ele_ymd_l
     
 
-def need_calculates_one_ele(pref_1,asitis_df,ele_num):
-    
-    ele_ymd_l = asitis_df["ele_ymd"].values
-    
+def need_calculates_one_ele(pref_1,ele_ymd_l,ele_num):
     #選挙名+ymd
     ele_ymd = ele_ymd_l[ele_num]
     
@@ -43,7 +41,7 @@ def need_calculates_one_ele(pref_1,asitis_df,ele_num):
     #候補者の基本変数に欠損があるかどうか
     nan_bools = target_df[needed_variables_l].isnull().any()
     #当選者の基本変数に欠損があるかどうか
-    nan_bools_win = target_df[target["win"] == True][needed_variables_l].isnull().any()
+    nan_bools_win = target_df[target_df["win"] == True][needed_variables_l].isnull().any()
     
     #選挙区数
     if nan_bools["district"] == False:
@@ -61,7 +59,7 @@ def need_calculates_one_ele(pref_1,asitis_df,ele_num):
         n_win_data = np.nan
 
     #選挙ごとの競争率
-    compe_rate_data = n_cand_data/n_wins
+    compe_rate_data = n_cand_data/n_wins_data
     
     #taeget_df に無投票情報の情報を追加　
     target_df["no_vote"] = 0
@@ -71,10 +69,10 @@ def need_calculates_one_ele(pref_1,asitis_df,ele_num):
     
     #無投票の場合情報が載っているはずの変数 "d_voting_rate" が欠損になっている可能性も考慮
     if target_df["d_voting_rate"].isnull().any() :
-        dvr_nans = target_df[target_df["d_voting_rate"].isnull()]　# "d_voting_rate"が欠損値になっている候補者
-        compe_d = target_df.groupby("district").apply(lambda d: d.id.count()/d.win.sum())　#候補者データに基づく各選挙区の競争率
+        dvr_nans = target_df[target_df["d_voting_rate"].isnull()] #"d_voting_rate"が欠損値になっている候補者
+        compe_d = target_df.groupby("district").apply(lambda d: d.id.count()/d.win.sum()) #候補者データに基づく各選挙区の競争率
         minasi_nv_d_l = list(compe_d[compe_d <= 1].keys()) #無投票の選挙区リスト
-        target_df.loc[dvr_nans[dvr_nans["district"].isin(minasi_nv_d_l)].index,"no_vote"] = 1　# d_voting_rate"が欠損かつ無投票選挙区の場合無投票に
+        target_df.loc[dvr_nans[dvr_nans["district"].isin(minasi_nv_d_l)].index,"no_vote"] = 1 # d_voting_rate"が欠損かつ無投票選挙区の場合無投票に
         target_df.loc[dvr_nans[dvr_nans["district"].isin(minasi_nv_d_l)].index,"minasi_no_vote"] = 1 #「みなし無投票」としておく
     
     else:
@@ -107,8 +105,8 @@ def need_calculates_one_ele(pref_1,asitis_df,ele_num):
         age_stats_wins[1] = target_df[target_df["win"] == True]["age"].std(ddof=0)
         
     else:
-        ave_age_cand = np.nan
-        ave_age_wins =np.nan
+        age_stats_cand = [np.nan]*7
+        age_stats_wins = [np.nan]*7
         
     
     #選挙ごとの女性割合
@@ -250,19 +248,20 @@ def need_calculates_one_ele(pref_1,asitis_df,ele_num):
     #選挙ごとの総有効得票数
     all_votes = target_df["n_votes"].sum()
 
+
     #選挙ごとの政党数
     n_circles = target_df.groupby("circle").size().size
                 
     #ここまでの変数をリスト化
-    calculates_l = [ele_ymd,n_districts,n_cand_data,n_wins_data,compe_rate_data,no_voting_ratio_win,no_voting_ratio_districs,minasi_nv_ratio_districts,ratio_women_cand,ratio_women_wins,ratio_inc_cand,,ratio_new_cand,ratio_form_cand,ratio_inc_wins,ratio_new_wins,ratio_form_wins,ratio_inc_votes,ratio_new_votes,ratio_form_votes,ratio_inc_votes_percand,ratio_new_votes_percand,ratio_form_votes_percand,all_votes,n_circles] + age_stats_cand + age_stats_wins
+    calculates_l = [ele_ymd,n_districts,n_cand_data,int(n_wins_data),compe_rate_data,no_voting_ratio_win,no_voting_ratio_districts,minasi_nv_ratio_districts,ratio_women_cand,ratio_women_wins,ratio_inc_cand,ratio_new_cand,ratio_form_cand,ratio_inc_wins,ratio_new_wins,ratio_form_wins,ratio_inc_votes,ratio_new_votes,ratio_form_votes,ratio_inc_votes_percand,ratio_new_votes_percand,ratio_form_votes_percand,all_votes,n_circles] + age_stats_cand + age_stats_wins
 
     return calculates_l
 
 
 def circle_info_all_eles(pref_1,asitis_df):
     
-    ele_ymd_l = asitis_df["ele_ymd"].values
-    target_df = pref_1[pref_1["ele_ymd"] == ele_ymd_l[ele_num]]
+    #ele_ymd_l = asitis_df["ele_ymd"].values
+    #target_df = pref_1[pref_1["ele_ymd"] == ele_ymd_l[ele_num]]
     
     #政党情報に欠損値が1つでもある選挙は除外
     no_nan = pref_1.groupby("ele_ymd").apply(lambda x:x.circle.isnull().any()==False)
@@ -270,7 +269,7 @@ def circle_info_all_eles(pref_1,asitis_df):
     pref_1 = pref_1[pref_1["ele_ymd"].isin(list(no_nan.keys()))] #欠損無し選挙名リストに入っていない選挙の候補者を除外
     
     #選挙ごとの政党別得票数
-    grouped = pref_1.groupby(["ele_ymd","circle"])["n_votes"].apply(pd.Series.sum,skipna=False)　#選挙別、政党別得票数の Series
+    grouped = pref_1.groupby(["ele_ymd","circle"])["n_votes"].apply(pd.Series.sum,skipna=False) #選挙別、政党別得票数の Series
     rowele_df = grouped.unstack(level=1) #各政党を列にした選挙別の df
     rowele_df["sum_v"] = rowele_df.sum(axis=1) #合計得票数
     
@@ -302,6 +301,25 @@ def circle_info_all_eles(pref_1,asitis_df):
     n_wins_df.columns = ["当選者数_" + i for i in list(n_wins_df.columns)]
     
     #concate
-    result_df = pd.concat([rowele_df,voteshare_df,voteshare_ave_df,n_cand_df,n_wins_df],axis=1,keys=["得票数","得票率","一人あたり得票率","立候補者数","当選者数"])
+    result_df = pd.concat([rowele_df,voteshare_df,voteshare_ave_df,n_cand_df,n_wins_df],axis=1)
     
     return result_df
+
+def built_elec_unit_for_pref(pref_num):
+    pref_1 = initial_setting(pref_num)
+    asitis_df, ele_ymd_l = asitis_all_eles(pref_1)
+    eles = asitis_df.shape[0]
+    circle_info_df = circle_info_all_eles(pref_1,asitis_df)
+    calculates_des = ["ele_ymd","n_districts","n_cand_data","n_wins_data","compe_rate_data","no_voting_ratio_win","no_voting_ratio_districts","minasi_nv_ratio_districts","ratio_women_cand","ratio_women_wins","ratio_inc_cand","ratio_new_cand","ratio_form_cand","ratio_inc_wins","ratio_new_wins","ratio_form_wins","ratio_inc_votes","ratio_new_votes","ratio_form_votes","ratio_inc_votes_percand","ratio_new_votes_percand","ratio_form_votes_percand","all_votes","n_circles","age_mean_cand","age_std_cand","age_min_cand","age_1q_cand","age_median_cand","age_3q_cand","age_max_cand","age_mean_wins","age_std_wins","age_min_wins","age_1q_wins","age_median_wins","age_3q_wins","age_max_wins"]
+    cal_l_l = []
+    for i in range(eles):
+        cal_l = need_calculates_one_ele(pref_1,ele_ymd_l,i)
+        cal_l_l.append(cal_l)
+    cal_df = pd.DataFrame(cal_l_l,columns=calculates_des)
+    cal_df.index = cal_df["ele_ymd"]
+    elec_df = pd.concat([asitis_df,cal_df,circle_info_df],axis=1)
+    elec_df.to_csv("elec_unit_data/pref_"+str(pref_num)+"_gikai_elections.csv")
+    
+if __name__ == '__main__':
+    for pref_id in range(1,48):
+        built_elec_unit_for_pref(pref_id)
